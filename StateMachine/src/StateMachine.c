3baca18441c888faab861d95afdc55da2a9f7b0e
gokhan <gokhan@gokhan-VPCEB4M1E>
2018-07-17 22:44:39 +0300

    f

--- StateMachine/src/StateMachine.c
+++ StateMachine/src/StateMachine.c
@@ -13,65 +13,136 @@
 #include "time.h"
 
 typedef enum{
-	STATE_A,
-	STATE_B,
-	STATE_C,
-	STATE_D,
+	FALSE=0,
+	TRUE=!FALSE,
+}State_t;
+
+#define __IS_NEUTRAL_POSITION() (TRUE)
+#define __MAX_PERCENT(A,B)	(A*(1+B/100.0))
+#define __MIN_PERCENT(A,B)	(A*(1-(B/100.0)))
+
+#define __IS_BETWEEN_TOLERANCE(A,B,C) ((A<=__MAX_PERCENT(B,C))?((A>=__MIN_PERCENT(B,C)?TRUE:FALSE)):FALSE)
+
+typedef enum{
+	STATE_NEUTRAL,
+	STATE_NORTH,
+	STATE_SOUTH,
+	STATE_WEST,
+	STATE_EAST,
+	STATE_NORTH_WEST,
+	STATE_NORTH_EAST,
+	STATE_SOUTH_WEST,
+	STATE_SOUTH_EAST,
 	STATE_LAST,
 }StateType_t;
+
+typedef enum{
+	JOYSTICK_TWO_AXIS,
+	JOYSTICK_ONE_AXIS_NORTH_SOUTH,
+	JOYSTICK_ONE_AXIS_WEST_EAST,
+	JOYSTICK_LAST,
+}JoystickAxisType_t;
+
+typedef enum{
+	ANALOG_8_32V,
+	ANALOG_RATIOMETRIC,
+	ANALOG_URRENT_OUT,
+	CAN_J1939,
+	CAN_OPEN,
+}JoystickBaseType_t;
 
 typedef struct{
 	StateType_t State;
 	void (*func)(void);
 }StateMachine_t;
 
-StateType_t StateCurrent=STATE_A;
-StateType_t StateNext=STATE_A;
+StateType_t StateCurrent=STATE_NEUTRAL;
+StateType_t StateNext=STATE_NEUTRAL;
 int DelayTiming=0;
 int CurrentTime=0;
+int JoystickAxisType=JOYSTICK_TWO_AXIS;
+int JosytickBaseType = CAN_J1939;
 
-void State_A(void);
-void State_B(void);
-void State_C(void);
-void State_D(void);
+void StateNeutral(void);
+void StateNorth(void);
+void StateSouth(void);
+void StateEast(void);
+void StateNorthWest(void);
+void StateNorthEast(void);
+void StateSouthWest(void);
+void StateSouthEast(void);
 void StateDelay(void);
 
-void State_A(void){
-	StateCurrent = STATE_A;
-	printf("!!!State A\n");
+typedef struct{
+	struct{
+		int reel;
+		int comp;
+	}x_axis,y_axis;
+}JoystickAxisValue_t;
+
+JoystickAxisValue_t JoystickAxis;
+
+void StateNeutral(void){
+	JoystickAxisValue_t axis;
+
+	StateCurrent = STATE_NEUTRAL;
+
+	if(__IS_NEUTRAL_POSITION() == FALSE){
+		return;
+	}
+
+	// disable interrupt
+	axis=JoystickAxis;
+	int tol=__IS_BETWEEN_TOLERANCE(4950,5000,1);
+	printf("!!!State A = %d \n",tol);
+	// enable interrupt
+
+	//axis check
+
+	if (JoystickAxisType == JOYSTICK_TWO_AXIS) {
+
+		switch (JosytickBaseType) {
+		case CAN_J1939:
+
+			printf("!!!State A\n");
+
+			break;
+		default:
+			break;
+		}
+	}
 
 	DelayTiming=2;
-	StateNext=STATE_B;
+	StateNext=STATE_NORTH;
 }
 
-void State_B(void){
-	StateCurrent = STATE_B;
+void StateNorth(void){
+	StateCurrent = STATE_NORTH;
 	printf("!!!State B\n");
 
 	DelayTiming=5;
-	StateNext=STATE_C;
+	StateNext=STATE_SOUTH;
 }
 
-void State_C(void){
-	StateCurrent = STATE_C;
+void StateSouth(void){
+	StateCurrent = STATE_SOUTH;
 	printf("!!!State C\n");
 
 	DelayTiming=3;
-	StateNext=STATE_D;
+	StateNext=STATE_WEST;
 }
 
-void State_D(void){
-	StateCurrent = STATE_D;
+void StateEast(void){
+	StateCurrent = STATE_WEST;
 	printf("!!!State D\n");
 
 	DelayTiming=8;
-	StateNext=STATE_A;
+	StateNext=STATE_NEUTRAL;
 }
 
 void StateDelay(void){
-	if(DelayTiming!=0){
-		if(CurrentTime+DelayTiming>(clock()/CLOCKS_PER_SEC))
-			return;
+	if(DelayTiming!=0 && CurrentTime+DelayTiming>(clock()/CLOCKS_PER_SEC)){
+		return;
 	}
 
 	printf("Different Time = %d\n",(int)(clock()/CLOCKS_PER_SEC)-CurrentTime);
@@ -80,14 +151,14 @@ void StateDelay(void){
 }
 
 StateMachine_t StateMachine[]={
-		{ STATE_A, State_A },
-		{ STATE_B, State_B },
-		{ STATE_C, State_C },
-		{ STATE_D, State_D },
+		{ STATE_NEUTRAL, StateNeutral },
+		{ STATE_NORTH, StateNorth },
+		{ STATE_SOUTH, StateSouth },
+		{ STATE_WEST, StateEast },
 };
 
 void StateRun(void){
-	if(StateCurrent<STATE_LAST && DelayTiming==0){
+	if(StateCurrent<STATE_NORTH_WEST && DelayTiming==0){
 		(*StateMachine[StateCurrent].func)();
 		CurrentTime=clock()/CLOCKS_PER_SEC;
 	}else{
